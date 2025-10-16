a8831ff14aa58013e6f6a542d57b30c32f7e496d
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-10-16 21:50:00 +0530

    made a submit button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,19 @@ import react, {useState} from 'react';
 
 function App() {
     const [Prompt,setPrompt] = useState('');
+
     function prompt_change(e){
         setPrompt(e.target.value);
     }
+
+    function button_click(){
+        setPrompt('');
+    }
+
     return (
         <>
             <input onChange={prompt_change} value={Prompt}/>
+            <button onClick={button_click}>submit</button>
         </>
     )
 }
