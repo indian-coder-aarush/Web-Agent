68e9a3da8713dfbfc0e40cb88919d55c8e6f3db7
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-10-28 21:34:52 +0530

    frontend now recieves ai response

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
 import react, {useState, useEffect} from 'react';
-import io from 'socket.io-client';
+import {io} from 'socket.io-client';
+
+const socket = io('http://localhost:1235');
 
 function App() {
 
-    //const socket = io('http://localhost:1234');
-
     const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        socket.on("Message", (message) => {
+            setMessages((prev) => [...prev, message]);
+        })
+        socket.on("reading file", (data) => {
+            let message = 'Reading File: ';
+            console.log(data);
+            message += data.content + '\n';
+            message += "Reason: " + data.reason + "\n";
+            message = {Role: "Agent", Message: message};
+            setMessages((prev) => [...prev, message]);
+        });
+        socket.on("writing file", (data) => {
+            console.log(data);
+            let message = 'Changing File: ' + data.File + '\n';
+            message += "Code Changed:" + data.content + '\n';
+            message += "Reason: " + data.reason + "\n";
+            message = {Role: "Agent", Message: message};
+            setMessages((prev) => [...prev, message]);
+        });
+        socket.on("running command", (data) => {
+            console.log(data);
+            let message = 'Running Command: ' + data.content + '\n';
+            message += "Reason: " + data.reason + "\n";
+            message = {Role: "Agent", Message: message};
+            setMessages((prev) => [...prev, message]);
+        });
+    },[]);
 
     const [Prompt,setPrompt] = useState('');
 
@@ -14,7 +43,7 @@ function App() {
     }
 
     async function send_prompt(){
-        await fetch("http://127.0.0.1:1234///api/receive-prompt", {
+        await fetch("http://127.0.0.1:1235/api/receive-prompt", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ prompt: Prompt })
